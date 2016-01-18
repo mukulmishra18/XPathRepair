c00fdc338cfa49c5a51dfeb0901eb03b2a7c035e
Preetwinder Singh Baath <preetwinder14496@gmail.com>
2016-01-18 21:53:35 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -10,27 +10,27 @@ class XPathRepair():
 
     def process(self, field, xpath):
         field = Field(field, xpath)
-        self.Generate(field)
-        if self.IsAnomaly(field.xpath):
-            field = self.Repair(field)
-        return self.Extract(field)
+        self.generate(field)
+        if self.isAnomaly(field.xpath):
+            field = self.repair(field)
+        return self.extract(field)
 
-    def Generate(self, field):
+    def generate(self, field):
         if isfile(self.XPathFilename):
             return
         GenerateXPaths(self.response, field, self.XPathFilename)
         
-    def Extract(self, field):
+    def extract(self, field):
         return self.response.xpath(field.xpath.xpath).extract()
 
-    def Repair(self, field):
+    def repair(self, field):
         with open(self.XPathFilename, 'r') as f:
             for line in f:
-                if not self.IsAnomaly(XPath(line)):
+                if not self.isAnomaly(XPath(line)):
                     return Field(field.field, XPath(line))
             return field
                     
-    def IsAnomaly(self, xpath):
+    def isAnomaly(self, xpath):
         if self.response.xpath(xpath.xpath).extract() == []:
             return True
         return False
